d4174eaa903816cb02d330febbbf5fb62495a0ac
sonuyes <129366264+sonuyes@users.noreply.github.com>
2025-07-24 19:36:36 +0530

    Update consumers.py

--- django-channels-basic-chat-master/chat/consumers.py
+++ django-channels-basic-chat-master/chat/consumers.py
@@ -3,8 +3,6 @@ import os
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
 from datetime import datetime
-import git  # Make sure GitPython is installed
-
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
@@ -30,13 +28,15 @@ class ChatConsumer(WebsocketConsumer):
         data = json.loads(text_data)
         msg_id = data.get("id")
         message_type = data.get("type", "chat")
+        message_file = os.path.join(os.path.dirname(__file__), 'messages.json')
 
         if message_type == "delete":
+            # Remove from JSON
             messages = self.load_messages()
             messages = [msg for msg in messages if msg['id'] != msg_id]
             self.save_messages(messages)
-            self.push_to_github()
 
+            # Broadcast delete
             async_to_sync(self.channel_layer.group_send)(
                 self.room_group_name,
                 {
@@ -56,8 +56,8 @@ class ChatConsumer(WebsocketConsumer):
         messages = self.load_messages()
         messages.append(new_msg)
         self.save_messages(messages)
-        self.push_to_github()
 
+        # Broadcast new message
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
@@ -91,24 +91,3 @@ class ChatConsumer(WebsocketConsumer):
         path = os.path.join(os.path.dirname(__file__), 'messages.json')
         with open(path, 'w', encoding='utf-8') as f:
             json.dump(messages, f, indent=2)
-
-    def push_to_github(self):
-        try:
-            repo_path = os.path.dirname(__file__)
-            repo = git.Repo(repo_path, search_parent_directories=True)
-            repo.git.add('*.json')
-            repo.index.commit("Update messages.json from Render")
-
-            github_token = os.environ.get("GITHUB_TOKEN")
-            if not github_token:
-                print("GitHub token missing")
-                return
-
-            # ⚠️ CHANGE THIS TO YOUR REPO URL
-            origin_url = f"https://{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}@github.com/sonuyes/chat-app.git"
-
-            origin = repo.remote(name='origin')
-            origin.set_url(origin_url)
-            origin.push()
-        except Exception as e:
-            print("GitHub push failed:", str(e))
